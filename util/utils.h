e7b96e80e67dbf62017637bc396ddd0dc8eb6fd1
ftk <@>
2011-12-04 00:24:32 +0400

    add const_hash

--- util/utils.h
+++ util/utils.h
@@ -12,6 +12,13 @@ typedef unsigned long hash_t;
 hash_t hash(const char *);
 hash_t hash(const std::string &);
 
+// http://stackoverflow.com/questions/2111667/compile-time-string-hashing
+inline constexpr hash_t const_hash(const char *s, int off = 0) 
+{
+    return !s[off] ? 5381 : (const_hash(s, off+1)*33) ^ s[off];
+}
+
+
 
 typedef unsigned long walltime_t;
 walltime_t get_walltime();
@@ -19,6 +26,7 @@ walltime_t get_walltime_s(); // in seconds
 
 
 void sleep(long ms);
+
 
 
 }
